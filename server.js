2f4e1e2bb99a3c001791e529338d3ad0dcba15b0
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-04-11 15:13:27 +0800

    created express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (Request, response) => {
+  response.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
