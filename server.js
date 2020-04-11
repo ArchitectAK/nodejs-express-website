151c9014dd1e4e8707feed974b1ca47857504df3
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-04-11 15:38:27 +0800

    serving html pages

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (Request, response) => {
-  response.send("Hello World");
+  response.sendFile(path.join(__dirname, "./static/index.html"));
+});
+app.get("/speakers", (Request, response) => {
+  response.sendFile(path.join(__dirname, "./static/speakers.html"));
 });
 
 app.listen(port, () => {
