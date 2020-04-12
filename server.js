e4465445770388465f99854425b3d6609de8bc1b
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-04-12 14:22:37 +0800

    congifured ejs template engine

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (Request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  // response.sendFile(path.join(__dirname, './static/index.html'));
+  response.render('pages/index', { pageTitle: ' Welcome' });
 });
 app.get('/speakers', (Request, response) => {
   response.sendFile(path.join(__dirname, './static/speakers.html'));
