135ec9583ed7c4945c8b12673cd1a026c0f403eb
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-04-12 13:09:19 +0800

    added eslint and prettier

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (Request, response) => {
-  response.sendFile(path.join(__dirname, "./static/index.html"));
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (Request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
 });
-app.get("/speakers", (Request, response) => {
-  response.sendFile(path.join(__dirname, "./static/speakers.html"));
+app.get('/speakers', (Request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
