a295fdaab00bb131e92c30fd8e59e189869c33ef
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-04-14 20:30:14 +0800

    added all subroutes

---
+++ routes/feedback.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = () => {
+  router.get('/', (request, response) => {
+    response.send('Feedback page');
+  });
+
+  router.post('/', (request, response) => {
+    response.send('Feedback form posted');
+  });
+
+  return router;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const speakesRoutes = require('./speakers');
+const feedbackRoutes = require('./feedback');
+
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (Request, response) => {
+  router.get('/', (request, response) => {
     response.render('pages/index', { pageTitle: ' Welcome' });
   });
+
+  router.use('/speakers', speakesRoutes());
+  router.use('/feedback', feedbackRoutes());
   return router;
 };
